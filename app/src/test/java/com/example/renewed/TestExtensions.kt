d926740117efe02bcdbacc4296091ff229350b70
David J. Kennedy <davidkennedyster@gmail.com>
2022-06-02 23:19:08 -0700

    added json responses to reddit requests as text files to use for mockwebserver in unit tests

---
+++ app/src/test/java/com/example/renewed/TestExtensions.kt
@@ -0,0 +1,21 @@
+package com.example.renewed
+
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
+import java.nio.charset.StandardCharsets
+
+fun MockWebServer.enqueueResponse(fileName: String, code: Int) {
+
+    val inputStream = this.javaClass.classLoader!!.getResource("hundredcomments.json")
+        .openStream()
+    val source = inputStream?.let { inputStream.source().buffer() }
+    source?.let {
+        enqueue(
+            MockResponse()
+                .setResponseCode(code)
+                .setBody(source.readString(StandardCharsets.UTF_8))
+        )
+    }
+}
