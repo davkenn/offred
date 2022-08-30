45445fc0c1e9b91ab73502850262aa675b5be7aa
David J. Kennedy <davidkennedyster@gmail.com>
2022-08-30 07:58:52 -0700

    got some tests working

--- app/src/androidTest/java/com/example/renewed/LargeTest.kt
+++ app/src/androidTest/java/com/example/renewed/LargeTest.kt
@@ -1,7 +1,11 @@
 package com.example.renewed
 
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import com.example.renewed.models.FullViewState
+import com.example.renewed.models.MyEvent
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -16,13 +20,23 @@ class LargeTest {
     @Inject
     lateinit var rep:BaseSubredditsAndPostsRepo
 
+    lateinit var vm :SubredditsAndPostsVM
+
+
     @Before
     fun init() {
         hiltRule.inject()
+         vm = SubredditsAndPostsVM(rep)
+        vm.prefetch().subscribe()
+
     }
 
     @Test
     fun get_this(){
+        var l = vm.vs.test()
+        vm.processInput(MyEvent.ScreenLoadEvent(""))
+
+        assertThat("hello",l.values().first().toString(), equalTo("Aaaa") )
         assert(rep is SubredditsAndPostsRepository)
 
     }
