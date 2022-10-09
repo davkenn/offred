45a99be759a9091c77d86bb838f7865f2e9caaa9
David J. Kennedy <davidkennedyster@gmail.com>
2022-10-08 20:11:04 -0700

    first step towards also working offline

--- app/src/main/java/com/example/renewed/DefaultDBContents/RoomT3DefaultData.kt
+++ app/src/main/java/com/example/renewed/DefaultDBContents/RoomT3DefaultData.kt
@@ -4,6 +4,6 @@ import com.example.renewed.models.RoomT3
 import java.time.Instant
 
 var t3SampleList = listOf(
-RoomT3(name = "t3_hjojh", subredditId = "t5_hjigj", created_utc = Instant.now(),
+RoomT3(name = "t3_hjojh", subredditId = "t5_tuhj3", created_utc = Instant.now(),
          timeLastAccessed = Instant.now(), title ="this sucjd",url ="", permalink ="",
                                      thumbnail ="",selftext=""))
--- app/src/main/java/com/example/renewed/DefaultDBContents/RoomT5DefaultData.kt
+++ app/src/main/java/com/example/renewed/DefaultDBContents/RoomT5DefaultData.kt
@@ -6,7 +6,30 @@ import java.time.Instant
 var t5SampleList = listOf(
     RoomT5(
 name = "t5_tu4j3",
-displayName = "Fun with roses",
+displayName = "Funwithroses",
+description ="A great site",
+thumbnail ="",
+banner_img ="",
+created_utc = Instant.now(),
+timeLastAccessed = Instant.now(),
+subscribers = 8,
+isSaved = false,
+totalViews = 0),
+    RoomT5(
+        name = "t5_tuhj3",
+        displayName = "Funwithses",
+        description ="A great site",
+        thumbnail ="",
+        banner_img ="",
+        created_utc = Instant.now(),
+        timeLastAccessed = Instant.now(),
+        subscribers = 8,
+        isSaved = false,
+        totalViews = 0)
+,
+RoomT5(
+name = "t5_t88jk",
+displayName = "Funwithroses",
 description ="A great site",
 thumbnail ="",
 banner_img ="",
@@ -16,3 +39,4 @@ subscribers = 8,
 isSaved = false,
 totalViews = 0)
 )
+
