eaa70b1520ef95e5f0da137da3436fe0bf5fdbe8
David J. Kennedy <davidkennedyster@gmail.com>
2023-01-26 08:14:30 -0800

    fixed a viewpagerpagestotal variable

--- app/src/main/java/com/example/renewed/Constants.kt
+++ app/src/main/java/com/example/renewed/Constants.kt
@@ -1,9 +1,12 @@
 package com.example.renewed
 
-
-//SCREEN 2
+//SCREEN 1
 const val REPEATS = 1
 const val PAGE_COUNT = 12
 const val SCREEN1_DB_SIZE = 80
+
+
+//SCREEN 2
+//TODO doesn't work yet if set to 10 and 20
 const val VIEWPAGER_PAGES_TOTAL = 12
 const val VP_PAGES_PER_LOAD = 6
--- app/src/main/java/com/example/renewed/Screen2/FavoritesListAdapter.kt
+++ app/src/main/java/com/example/renewed/Screen2/FavoritesListAdapter.kt
@@ -6,6 +6,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.renewed.Screen1.PostsAdapter
 import com.example.renewed.Screen1.Subscreen.BlankFragment
 import com.example.renewed.Screen1.Subscreen.PostFragment
+import com.example.renewed.VIEWPAGER_PAGES_TOTAL
 import com.example.renewed.models.PartialViewState
 import com.example.renewed.models.ViewStateT3
 import com.example.renewed.models.isVideoPost
@@ -15,8 +16,8 @@ import timber.log.Timber
 
 class FavoritesListAdapter(private val fragment: FavoritesListFragment): FragmentStateAdapter(fragment) {
     var postIds: MutableList<String> = mutableListOf<String>()
-    var fragList: MutableList<PostFragment?> = arrayOfNulls<PostFragment>(12).toMutableList()
-    var a = arrayOfNulls<PostFragment>(12).toMutableList()
+    var fragList: MutableList<PostFragment?> = arrayOfNulls<PostFragment>(VIEWPAGER_PAGES_TOTAL).toMutableList()
+    var a = arrayOfNulls<PostFragment>(VIEWPAGER_PAGES_TOTAL).toMutableList()
 
     override fun getItemCount(): Int = postIds.size
     override fun getItemId(position: Int): Long = postIds[position].hashCode().toLong()
