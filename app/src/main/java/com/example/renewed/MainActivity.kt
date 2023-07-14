45bac04866afbfb61ae3c7e84a7a91983586e253
David J. Kennedy <davidkennedyster@gmail.com>
2023-07-13 18:45:37 -0700

    loaded token into session manager

--- app/src/main/java/com/example/renewed/MainActivity.kt
+++ app/src/main/java/com/example/renewed/MainActivity.kt
@@ -30,7 +30,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     @Inject lateinit var auth:AuthAPI
     private lateinit var navController: NavController
     private lateinit var bottomNavigationView: BottomNavigationView
-    private lateinit var token:String
+    private var token:String=""
+    private var sm:SessionManager?=null
 
      fun login(): Single<String> {
         val credentials = "u3MaMah0dOe1IA:"
@@ -38,13 +39,16 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         val encodedCredentials: String = Base64.encodeToString(credentials.toByteArray(), Base64.NO_WRAP)
 
         auth.installedClient(   "Basic $encodedCredentials",     "https://oauth.reddit.com/grants/installed_client",
-            "DO_NOT_TRACK_THIS_DEViCE").subscribeBy { token=it.getOrDefault("access_token","") }
+            "DO_NOT_TRACK_THIS_DEViCE").subscribeBy { token=it.getOrDefault("access_token","");sm?.saveAuthToken(token) }
         return Single.just("AAAA")
     }
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+        sm= SessionManager(this)
         login().subscribe()
+        sm?.saveAuthToken(token)
+
         Timber.d("onCreate called")
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
