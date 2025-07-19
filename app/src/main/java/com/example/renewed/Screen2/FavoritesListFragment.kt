4f147ab735b376975a76c5d3c6f5f3f3b167cd93
= <davidkennedyster@gmail.com>
2025-07-19 14:28:02 -0700

    fixed bug where clicking a link to open a web browser from the infinite list in Screen2 did not maintain infinite list state when coming back from the browser

--- app/src/main/java/com/example/renewed/Screen2/FavoritesListFragment.kt
+++ app/src/main/java/com/example/renewed/Screen2/FavoritesListFragment.kt
@@ -82,6 +82,9 @@ class FavoritesListFragment : Fragment(R.layout.fragment_favorites_list) {
                         Screen2Effect.LOAD -> {
                             showLoading()
                         }
+                        Screen2Effect.LOAD_DONE -> {
+                            hideLoading()
+                        }
 //TODO fix this
                         else -> {}
                     }
@@ -101,6 +104,7 @@ class FavoritesListFragment : Fragment(R.layout.fragment_favorites_list) {
         vp.pageSelections().subscribe { position -> Timber.d("THELIISPOS $position")
             //update position if loading new posts for new pages in infinite list
             if (position == VP_PAGES_PER_LOAD+2) {
+                showLoading()
                 favoritesVM.processInput(Screen2Event.UpdatePositionEvent(2))
                 favoritesVM.processInput(
                     Screen2Event.LoadMoreEvent(vpPagesAdapter.postIds.take(VP_PAGES_PER_LOAD)))
